062fad2d8b691e0cd944a810366d22336bc08493
Evan Wallace <evan.exe@gmail.com>
2020-01-26 12:10:49 -0800

    allow lexical declarations inside "declare"

--- src/esbuild/parser/parser.go
+++ src/esbuild/parser/parser.go
@@ -3275,11 +3275,17 @@ func (p *parser) parseStmt(opts parseStmtOpts) ast.Stmt {
 					p.skipTypeScriptTypeStmt()
 					return ast.Stmt{loc, &ast.STypeScript{}}
 
-				case "namespace", "abstract", "declare":
+				case "namespace", "abstract":
 					// "export namespace Foo {}"
 					// "export abstract class Foo {}"
+					opts.isExport = true
+					return p.parseStmt(opts)
+
+				case "declare":
 					// "export declare class Foo {}"
 					opts.isExport = true
+					opts.allowLexicalDecl = true
+					opts.isTypeScriptDeclare = true
 					return p.parseStmt(opts)
 				}
 			}
@@ -3962,10 +3968,9 @@ func (p *parser) parseStmt(opts parseStmtOpts) ast.Stmt {
 
 					case "declare":
 						// "declare const x: any"
-						p.parseStmt(parseStmtOpts{
-							isTypeScriptDeclare: true,
-							allowLexicalDecl:    opts.allowLexicalDecl,
-						})
+						opts.allowLexicalDecl = true
+						opts.isTypeScriptDeclare = true
+						p.parseStmt(opts)
 						return ast.Stmt{loc, &ast.STypeScript{}}
 					}
 				}
